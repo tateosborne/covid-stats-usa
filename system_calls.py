04ee77da5e42613cf1ff0d8e83a348561aab04a3
Mac5564 <jack.mccallum64@gmail.com>
2022-01-31 17:36:33 -0500

    adding system_calls

---
+++ system_calls.py
@@ -0,0 +1,11 @@
+
+def help_user():
+    print("------Possible Commands------")
+    print("Start with the data you're trying to find.\n")
+    print("cases - deaths - mortality\n")
+    print("Then, chose how broad of a scope you want the data.")
+    print("total - state")
+    print("total = nationwide, state = state + abbreviation\n")
+    print("Then finally, if you want to search for a specific county, name it.")
+    print("NOTE: This is optional, you can search for statewide data instead.")
+    print("County ")
