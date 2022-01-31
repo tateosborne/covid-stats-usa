687604a9b0a04cac00badcab99b8313fa25029c0
Mac5564 <jack.mccallum64@gmail.com>
2022-01-31 18:47:30 -0500

    finished first draft of help call, made main more intuitive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Function that returns a string which is the numbered word of a larger string seperated by spaces
+#TODO: add calls for date & exit within main, it's pointless to have those in a separate file like the help call
+
 def parse_word(string, num_word):
     for i in num_word:
         hit_space = False
@@ -7,11 +9,15 @@ def parse_word(string, num_word):
             letter = 
             out_string +=
 
-    hit_space = False:
+    hit_space = False
 if __name__ == '__main__':
-    running = True; #Variable to hold whether the user wants to be playing.
+    running = True #Variable to hold whether the user wants to be playing.
+    print("Welcome to our COVID data searcher!")
+    print('Type "load data" if this is your first time running this.')
+    print('Type "help" for a how-to guide on structuring queries.\n')
     while running:
         user_input = input('Enter your Query: ')
-
+        # NOTE: NO REAL SWITCH STATEMENT IN PYTHON 3.9
+        # Dictionaries maybe?
 
     # commit test
