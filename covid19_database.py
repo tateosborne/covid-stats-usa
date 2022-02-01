a61a891972c8eb57f6a587cb56f3512a1491a464
Josh Baker <jjbaker@uvm.edu>
2022-02-01 15:51:07 -0500

    Wrote script for adding contents of CSV files to database

--- covid19_database.py
+++ covid19_database.py
@@ -15,17 +15,61 @@ def create_connection(db_file):
         # Create the table of county statistics
         # Table parameters are county, state, deaths
         c.execute('''
-        CREATE TABLE IF NOT EXISTS County_Data 
-        ([county] TEXT, [state] TEXT, [cases] INTEGER PRIMARY KEY, [deaths] INTEGER PRIMARY KEY)
+            CREATE TABLE IF NOT EXISTS County_Data 
+            ([county] TEXT, [state] TEXT, [cases] INTEGER PRIMARY KEY, [deaths] INTEGER PRIMARY KEY)
         ''')
 
         # Create the table of state statistics
         # Table parameters are state, cases, deaths
         c.execute('''
-        CREATE TABLE IF NOT EXISTS State_Data
-        ([state] TEXT, [cases] INTEGER PRIMARY KEY, [deaths] INTEGER PRIMARY KEY)
+            CREATE TABLE IF NOT EXISTS State_Data
+            ([state] TEXT, [cases] INTEGER PRIMARY KEY, [deaths] INTEGER PRIMARY KEY)
         ''')
         conn.commit()
+    # If there is an error, display which one
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+# This function executes data entry from a csv file, line by line, into the database
+# Previously created. This allows us to adjust data and re-enter it, so long as
+# The CSV file format follows that of the DB credentials in the above method
+def insert_values(db_file, county_csv, state_csv):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+
+        # Now read in the csv file, split the data elements by line, and then
+        # by comma. Convert necessary list elements from strings to ints, and insert
+        # them into the table
+        infile_county = open(county_csv, 'r')
+        for line in infile_county:
+            split_str = line.split(',')
+            c.execute('''
+                INSERT INTO County_Data (county, state, cases, deaths)
+                
+                    VALUES
+                    (split_str[0], split_str[1], int(split_str[2]), int(split_str[3]))
+            ''')
+        # Close the file
+        infile_county.close()
+
+        # Repeat for the state csv_file
+        infile_state = open(state_csv, 'r')
+        for line in infile_state:
+            split_str = line.split(',')
+            c.execute('''
+                    INSERT INTO State_Data (state, cases, deaths)
+
+                        VALUES
+                        (split_str[0], int(split_str[1]), int(split_str[2]),)
+                    ''')
+        # Close the file
+        infile_state.close()
+
     # If there is an error, display which one
     except Error as e:
         print(e)
