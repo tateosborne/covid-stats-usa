846f28db52a06025cc8675e610f170ba844269af
Josh Baker <jjbaker@uvm.edu>
2022-02-01 15:24:29 -0500

    Added documentation to database.py, created new .csv files for operation

--- covid19_database.py
+++ covid19_database.py
@@ -1,11 +1,14 @@
 import sqlite3
 from sqlite3 import Error
 
+# This function connects to an SQLite database 'db_file'
 def create_connection(db_file):
     conn = None
     try:
+        # conn represents the database
         conn  = sqlite3.connect(db_file)
         print(sqlite3.version)
+    # If there is an error, display which one
     except Error as e:
         print(e)
     finally:
