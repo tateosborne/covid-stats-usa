693bf26fb16d264b7953c2be372a471bb57c37df
Mac5564 <jack.mccallum64@gmail.com>
2022-02-01 12:46:15 -0500

    added get_output to main

--- main.py
+++ main.py
@@ -6,11 +6,42 @@ def parse_word(string, num_word):
     for i in num_word:
         hit_space = False
         out_string = ""
-        while (hit_space == False):
+        while not hit_space:
             letter = 
             out_string +=
 
     hit_space = False
+
+def get_output(user_input_list):
+    input_size = len(user_input_list)
+
+    # checks for system calls by user
+    if input_size == 1:
+        if user_input_list[0] == 'help':
+            system_calls.help_user()
+        elif user_input_list[0] == 'quit':
+            quit()
+        elif user_input_list[0] == 'date':
+            # do we also want to have this print out our source?
+            # possibly worth changing name of command if we do that
+            print("Our data was collected on 2/18/2021")
+
+    if input_size == 2:
+        if user_input_list[0] == 'load' and user_input_list[1] == 'data':
+            print('Loading data...')
+            # TODO: follow up on the above print statement
+        elif user_input_list[0] == 'cases':
+            print('showing cases')
+        elif user_input_list[0] == 'deaths':
+            print('showing deaths')
+        elif user_input_list[0] == 'mortality':
+            print('showing mortality')
+        # TODO: Follow these statements down so it can support more possible phrases
+
+    # NOTE: NO REAL SWITCH STATEMENT IN PYTHON 3.9
+    # Dictionaries maybe?
+
+
 if __name__ == '__main__':
     running = True #Variable to hold whether the user wants to be playing.
 
@@ -22,31 +53,5 @@ if __name__ == '__main__':
         user_input = input('Enter your Query: ')
         # turns user input to lowercase and splits along spaces
         user_input_list = user_input.lower().split(" ")
+        # some_var? = get_output(user_input_list)
 
-        # do we want the below section to be in its own method? it may be easier to test that way.
-
-        # checks for system calls by user
-        if len(user_input_list) == 1:
-            if user_input_list[0] == 'help':
-                system_calls.help_user()
-            elif user_input_list[0] == 'quit':
-                quit()
-            elif user_input_list[0] == 'date':
-                # do we also want to have this print out our source?
-                # possibly worth changing name of command if we do that
-                print("Our data was collected on 2/18/2021")
-
-        if len(user_input_list) == 2:
-            if user_input_list[0] == 'load' and user_input_list[1] == 'data':
-                print('Loading data...')
-                # TODO: follow up on the above print statement
-            elif user_input_list[0] == 'cases':
-                print('showing cases')
-            elif user_input_list[0] == 'deaths':
-                print('showing deaths')
-            elif user_input_list[0] == 'mortality':
-                print('showing mortality')
-            # TODO: Follow these statements down so it can support more possible phrases
-
-        # NOTE: NO REAL SWITCH STATEMENT IN PYTHON 3.9
-        # Dictionaries maybe?
