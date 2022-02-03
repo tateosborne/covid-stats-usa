8d248cd39f64b6ba5c3abda1583bb77475380edd
nljacobson <nljacobs@uvm.edu>
2022-02-03 15:57:06 -0500

    began get_input

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import system_calls
 import covid19_database
 from os.path import exists
+def get_input(user_input_list, loaded):
+    datatype = ""
+    state = ""
+    county = ""
+    if user_input_list[0] == 'load' and user_input_list[1] == 'data':
+        if exists('covid_data.db'):
+            print('Loading data...')
+            covid19_database.insert_values()
+            print('Done!')
+            loaded = True
+    elif user_input_list[0] == 'quit':
+        print('Quitting')
+        quit()
+    elif loaded:
+        if (user_input_list[0] in 'cases', 'deaths', 'mortality') & (exists(user_input_list[1])):
+
 if __name__ == '__main__':
     running = True #Variable to hold whether the user wants to be playing.
 
@@ -12,6 +28,6 @@ if __name__ == '__main__':
         user_input = input('Enter your Query: ')
         # turns user input to lowercase and splits along spaces
         user_input_list = user_input.lower().split(" ")
-        print(get_input(user_input_list))
+        print(get_input(user_input_list, loaded))
 
 
