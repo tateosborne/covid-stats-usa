310d2603d5d321c8a7273c52bcd61dbee5c1769c
Mac5564 <jack.mccallum64@gmail.com>
2022-02-10 16:12:38 -0500

    fixed caps on state

--- main.py
+++ main.py
@@ -30,32 +30,42 @@ def get_input(user_input_list, loaded):
             if user_input_list[1] == 'total':
                 return covid19_database.make_queries(datatype, "", "")
             elif len(user_input_list) == 2:
-                return covid19_database.make_queries(datatype, get_state(user_input_list), "")
+                return covid19_database.make_queries(datatype, get_state(user_input_list).upper(), "")
             elif len(user_input_list) > 4:
-                return covid19_database.make_queries(datatype, get_state(user_input_list), get_county(user_input_list))
+                return covid19_database.make_queries(datatype, get_state(user_input_list).upper(), get_county(user_input_list))
     return "Incorrect Syntax, try again"
 
+
+# checks to see if state is formatted correctly, then separates for ease of use
 def get_state(user_input_list) -> str:
     state = user_input_list[2]
+    # checks length of state name to ensure validity
     if not len(state) == 2:
-        return 'State input should be in Abbreviated form (i.e. VT)'
+        print("State input should be in abbreviated form (i.e. VT)")
+        return ""
     return state
 
 
+# takes seperated names of counties, then returns to main with complete name
 def get_county(user_input_list) -> str:
+    # default value for county
     county = ""
-    for i in range(4, len(user_input_list)):
+    # runs through user_input_list from start of county name to end of list
+    for i in range(4, len(user_input_list)-1):
+        # for first part of county, no space for separation, but there is for rest
         if i == 4:
             county = user_input_list[i]
         else:
-            county += " "+user_input_list[i]
+            county += " " + user_input_list[i]
+    # case for invalid county entry
     if county == "":
+        print("Invalid county specified")
         return ""
     return county
 
 
 if __name__ == '__main__':
-    running = True #Variable to hold whether the user wants to be playing.
+    running = True  # Variable to hold whether the user wants to be playing.
 
     print("Welcome to our COVID data searcher!")
     print('Type "load" if this is your first time running this.')
@@ -66,5 +76,3 @@ if __name__ == '__main__':
         # turns user input to lowercase and splits along spaces
         user_input_list = user_input.lower().split(" ")
         print(get_input(user_input_list, loaded))
-
-
