a1f51af0822fe3fcf4b5dcf4f9f15792001e6556
nljacobs <nljacobs@uvm.edu>
2022-02-16 21:26:48 -0500

    Jason prevention handling

--- covid19_database.py
+++ covid19_database.py
@@ -127,7 +127,7 @@ def retrieve_data(query: str) -> str:
     conn = sqlite3.connect(db_file)
     # print(sqlite3.version)
     for c in query:
-        if (c == '%', ';'):
+        if (c == '%'):
             return "Please stop trying to break our code Jason, we're on to you"
     # Retrieve data from the desired table
     c = conn.cursor()
